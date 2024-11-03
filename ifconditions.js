a435c6ec49a634395c5ebf301021bdd1752ca3e8
Sundar <sundarapandianec@gmail.com>
2024-11-03 23:07:53 +0530

    first changes

---
+++ ifconditions.js
@@ -0,0 +1,26 @@
+let score = 95;
+
+if (score > 90) {
+    console.log("execellent");
+} else if (score < 90) {         //output execellent
+    console.log("good");
+} else {
+    console.log("better luck next time")
+}
+
+let fruit = "apple";
+
+if (fruit == "apple") {            //output matched
+    console.log("matched");
+} else {
+    console.log("not matched");
+}
+
+let fruit1 = "orange";
+
+if (fruit1 == "apple") {            //output  not matched
+    console.log("matched");
+} else {
+    console.log("not matched");
+}
+
